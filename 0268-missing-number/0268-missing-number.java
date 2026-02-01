31a8ba54a89866bd3676f6158b8e9d21a4ac2fa6
Aravindgit2005 <aravaravind2005@gmail.com>
2026-02-01 10:46:02 +0530

    Time: 0 ms (100%), Space: 47.3 MB (60.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
        int n=nums.length;
        int x1=0;
-       for(int i=1;i<=n;i++){
+       for(int i=0;i<=n;i++){
         x1^=i;
        }
        int x2=0;

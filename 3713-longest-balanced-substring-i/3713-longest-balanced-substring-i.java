89ce19a18a23709289da1e66cf1f38f1ccdacf93
Aravindgit2005 <aravaravind2005@gmail.com>
2026-02-13 14:38:24 +0530

    Time: 144 ms (51.5%), Space: 47.2 MB (27.38%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -1,22 +1,32 @@
 class Solution {
     public int longestBalanced(String s) {
-        int n = s.length();
-        int[] cnt = new int[26];
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            Arrays.fill(cnt, 0);
-            int mx = 0, v = 0;
-            for (int j = i; j < n; ++j) {
-                int c = s.charAt(j) - 'a';
-                if (++cnt[c] == 1) {
-                    ++v;
-                }
-                mx = Math.max(mx, cnt[c]);
-                if (mx * v == j - i + 1) {
-                    ans = Math.max(ans, j - i + 1);
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            int[] freq=new int[26];
+            for(int j=i;j<s.length();j++){
+                char x=s.charAt(j);
+                freq[x-'a']++;
+                if(isfreqsame(freq)){
+                    max=Math.max(max,j-i+1);
                 }
             }
+            
         }
-        return ans;
+        return max;
     }
+    public static boolean isfreqsame(int[] freq){
+        int value=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0)continue;
+
+            if(value==0){
+                value=freq[i];
+            }
+            else if(value!=freq[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
